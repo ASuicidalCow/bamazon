59836091ab5183cb7ba3685d070fcbf52d94b339
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-05-16 16:15:22 -0600

    updated with all table info

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -11,7 +11,25 @@ CREATE TABLE products (
     primary key(itemId)
 );
  insert into products(productName, departmentName, price, stockQuantity)
- VALUES ("Harry Potter: Complete 8-Film Collection [Blu-ray]", "Movies", "46.99", "8");
+ VALUES ("Harry Potter: Complete 8-Film Collection", "Movies", "46.99", "8");
+ insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Cuties", "Produce", "3.00", "10");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Cotton T-Shirt", "Clothing", "5.00", "30");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Printer Paper", "Office", "8.00", "13");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Keyboard & Mouse Combo", "Electronics", "43.99", "2");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Fidget Spinner", "MISC", "2.55", "55");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Microphone", "Electronics", "256.00", "10");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("The Matrix", "Movies", "7.99", "1");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Samurai Sword", "Misc", "6000.00", "200");
+  insert into products(productName, departmentName, price, stockQuantity)
+ VALUES ("Cabbage", "Produce", ".50", "10");
 
 SELECT 
     *
